d6fb32c530e92ef9510b10f4c2b5d9c5ff96317e
Year 12 <year12@mar-admins-MacBook-Air-3.local>
2021-11-25 09:49:12 +0000

    25/11/21

--- src/search/binary/binarySearch.ts
+++ src/search/binary/binarySearch.ts
@@ -2,6 +2,39 @@ import { NO_MATCH } from "../common"
 
 const binarySearch = (items: any[], item: any): number => {
 
+
+    let lowestPt : number = 0 
+    let highestPt : number = items.length - 1 
+    let result : number = 0
+
+    while (result == 0) {
+        let middlePt: number = ((lowestPt + highestPt)/2)
+
+        if (item === middlePt) {
+            result = middlePt
+        }
+
+        if (item > middlePt) { 
+            lowestPt = middlePt
+            middlePt = (lowestPt + highestPt)/2
+
+            if (item === middlePt) {
+                result = middlePt
+            }
+
+            else {
+                
+            }
+
+
+
+        }
+
+        
+    }
+
+
+
     return NO_MATCH;
 }
 
